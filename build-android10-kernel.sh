d34ff6491a115deff7ac89b17d44a494d1a2f166
Kepler Chan <1653946112@qq.com>
2023-07-04 09:38:15 +0000

    ci: add build android10

--- build-android10-kernel.sh
+++ build-android10-kernel.sh
@@ -2,7 +2,7 @@
 
 echo "Build Piexl3 android os: 10 kernel version:4.19.xx "
 
-apt-get update
+sudo apt-get update
 
 cd ..
 # mkdir ~/.bin
@@ -11,15 +11,21 @@ cd ..
 # PATH=~/.bin/repo
 
 # download kernel
+rm -rf android10-kernel
+echo "creating dir "
 mkdir android10-kernel
 cd android10-kernel
 
-sudo apt install repo bc   git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig -y
+sudo apt install repo bc   cpio kmod  git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig -y
 # doownload kernel code 
 echo "sync kernel code "
 
-repo  init --depth 1 -u https://aosp.tuna.tsinghua.edu.cn/kernel/manifest -b android-msm-crosshatch-4.9-android12
-
+#repo  init --depth 1 -u https://aosp.tuna.tsinghua.edu.cn/kernel/manifest -b android-msm-crosshatch-4.9-android12
+# android 12 kernel
+# repo  init --depth 1 -u https://android.googlesource.com/kernel/manifest -b android-msm-crosshatch-4.9-android12
+# android 10 kernel 
+repo  init --depth 1 -u https://android.googlesource.com/kernel/manifest -b android-msm-crosshatch-4.9-android10
 repo  sync -qcj20
 
-
+# 编译 注意这里的build/build.config 被修改了
+BUILD_CONFIG=build/build.config SKIP_DECONFIG=0 SKIP_MRPROPER=0 ARCH=arm64 ./build/build.sh -j20
